bb75e2a80f8a5493dcebb7673e814a0cb8872e1a
vishal-ahirwar <vishalchoudhry0091@gmail.com>
2025-02-18 01:29:24 +0530

    recursive function added with depth

--- src/main.cxx
+++ src/main.cxx
@@ -12,18 +12,41 @@
 namespace fs = std::filesystem;
 struct Info
 {
+    int _depth{};
+    std::string _file_name{};
     bool _is_dir{};
     std::string _path{};
-    std::string _file_name{};
+};
+void printAllDirsRecursive(const fs::path &current_path) {
+
 };
 
+void getAllDirsRecursive(const fs::path &current_path, std::vector<Info> &info, int depth = 0)
+{
+    ++depth;
+    if (!fs::exists(current_path))
+    {
+        depth=0;
+        return;
+    };
+    for (const auto &next : fs::directory_iterator(current_path))
+    {
+
+        info.push_back(Info{._depth = depth, ._file_name = next.path().filename().string(), ._is_dir = next.is_directory(), ._path = fs::absolute(next.path().lexically_normal()).string()});
+
+        if (next.is_directory())
+        {
+            getAllDirsRecursive(next.path(), info,depth);
+        }
+    }
+};
 void getAllDirs(const std::string &path, std::vector<Info> &dirs)
 {
     if (!fs::exists(path))
         return;
-    for (const auto &dir : fs::recursive_directory_iterator(path))
+    for (const auto &next : fs::recursive_directory_iterator(path))
     {
-        dirs.push_back(Info{._is_dir = dir.is_directory(), ._path = dir.path().string(), ._file_name = dir.path().filename().string()});
+        dirs.push_back(Info{._depth = 0, ._file_name = next.path().filename().string(), ._is_dir = false, ._path = fs::absolute(next.path().lexically_normal()).string()});
     };
 };
 int main(int argc, char *argv[])
@@ -34,16 +57,17 @@ int main(int argc, char *argv[])
         return 0;
     }
     std::vector<Info> dirs{};
-    getAllDirs(argv[1], dirs);
+    getAllDirsRecursive(argv[1], dirs,-1);
+    // getAllDirs(argv[1], dirs);
     for (const auto &dir : dirs)
     {
         if (dir._is_dir)
         {
-            fmt::print(fmt::emphasis::underline | fmt::fg(fmt::color::green), "Directory :\n\tPath : {},\n\tName : {}\n", dir._path, dir._file_name);
+            fmt::print(fmt::emphasis::bold | fmt::fg(fmt::color::green), "Depth : {} ,Path : {}\nName : {}\n",dir._depth, dir._path, dir._file_name);
         }
         else
         {
-            fmt::print(fmt::emphasis::faint | fmt::fg(fmt::color::white), "File :\n\tPath : {},\n\tName : {}\n", dir._path, dir._file_name);
+            fmt::print(fmt::emphasis::faint | fmt::fg(fmt::color::white), "Depth : {}, Path : {}\nName : {}\n",dir._depth ,dir._path, dir._file_name);
         }
     }
     return 0;
